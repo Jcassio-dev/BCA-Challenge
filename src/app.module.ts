b31cdb97df633bfbca763ecb3088bd0b1d33c07c
JCassio <josecassio2013@gmail.com>
2025-08-20 18:56:20 -0300

    refactor: coupling of database and log services into modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './infra/config/env.config';
 import { PinoLoggerService } from './infra/logger/logger.service';
-import { DatabaseService } from './infra/database/database.service';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { APP_FILTER } from '@nestjs/core';
 import { DomainValidationFilter } from './infra/exceptions/filters/domain-validation.filter';
+import { DatabaseModule } from './infra/database/database.module';
+import { LoggerModule } from './infra/logger/logger.module';
 
 @Module({
   imports: [
@@ -15,16 +16,15 @@ import { DomainValidationFilter } from './infra/exceptions/filters/domain-valida
       load: [appConfig],
     }),
     TransactionsModule,
+    DatabaseModule,
+    LoggerModule,
   ],
   controllers: [],
   providers: [
-    PinoLoggerService,
-    DatabaseService,
     {
       provide: APP_FILTER,
       useClass: DomainValidationFilter,
     },
   ],
-  exports: [PinoLoggerService, DatabaseService],
 })
 export class AppModule {}
