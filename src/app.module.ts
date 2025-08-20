655101bbd6d98bae4002961cc976a6617e91f3fd
JCassio <josecassio2013@gmail.com>
2025-08-20 19:35:58 -0300

    chore: remove unused import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { appConfig } from './infra/config/env.config';
-import { PinoLoggerService } from './infra/logger/logger.service';
 import { TransactionsModule } from './modules/transactions/transactions.module';
 import { APP_FILTER } from '@nestjs/core';
 import { DomainValidationFilter } from './infra/exceptions/filters/domain-validation.filter';
