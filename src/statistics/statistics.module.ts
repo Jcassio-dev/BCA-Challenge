d0801b865d30c5bed869e426016cf179b56ee8a4
JCassio <josecassio2013@gmail.com>
2025-08-21 19:56:21 -0300

    chore: add controller and use-case to module and import the transactions module

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { StatisticsController } from './http/statistics.controller';
+import { TransactionsModule } from 'src/modules/transactions/transactions.module';
+import { GetStatisticsUseCase } from './use-cases/get-statistics.use-case';
 
 @Module({
-  imports: [StatisticsModule],
-  controllers: [],
-  providers: [],
+  imports: [TransactionsModule],
+  controllers: [StatisticsController],
+  providers: [GetStatisticsUseCase],
 })
 export class StatisticsModule {}
