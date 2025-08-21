d542558498d19b8e7e6705f2bc48aa7aa9fb46f2
JCassio <josecassio2013@gmail.com>
2025-08-20 22:45:38 -0300

    feat: create 'create-transaction' dto

---
+++ src/modules/transactions/dto/create-transaction.dto.ts
@@ -0,0 +1,15 @@
+import { Type } from 'class-transformer';
+import { IsNumber, IsISO8601, Min } from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsNumber({}, { message: 'Amount must be a valid number' })
+  @Min(0, { message: 'Amount must be greater than or equal to 0' })
+  amount: number;
+
+  @IsISO8601(
+    { strict: true },
+    { message: 'Timestamp must be a valid ISO 8601 date' },
+  )
+  @Type(() => Date)
+  timestamp: Date;
+}
