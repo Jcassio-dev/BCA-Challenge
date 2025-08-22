5dd8492825ac5a242837cea8439760fae4b4370f
JCassio <josecassio2013@gmail.com>
2025-08-22 00:38:41 -0300

    refactor: change throttler implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { appConfig } from './infra/config/env.config';
 import { TransactionsModule } from './modules/transactions/transactions.module';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { DomainValidationFilter } from './infra/exceptions/filters/domain-validation.filter';
 import { DatabaseModule } from './infra/database/database.module';
 import { LoggerModule } from './infra/logger/logger.module';
 import { StatisticsModule } from './statistics/statistics.module';
 import { HealthModule } from './modules/health/health.module';
-import { ThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -17,18 +17,12 @@ import { ThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler';
       envFilePath: ['.env'],
       load: [appConfig],
     }),
-    ThrottlerModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService): ThrottlerModuleOptions => ({
-        throttlers: [
-          {
-            ttl: configService.get<number>('app.rateLimitWindow') ?? 60,
-            limit: configService.get<number>('app.rateLimitMax')!,
-          },
-        ],
-      }),
-    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: Number(process.env.RATE_LIMIT_WINDOW_MS) || 60000,
+        limit: Number(process.env.RATE_LIMIT_MAX) || 10,
+      },
+    ]),
     TransactionsModule,
     DatabaseModule,
     LoggerModule,
@@ -40,6 +34,10 @@ import { ThrottlerModule, ThrottlerModuleOptions } from '@nestjs/throttler';
     {
       provide: APP_FILTER,
       useClass: DomainValidationFilter,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
